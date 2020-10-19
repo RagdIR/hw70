491c93302a91dafd1b49d438f421d24fd510b995
RagdIR <ragdirstrogov@gmail.com>
2020-10-19 18:57:30 +0600

    Обновлены вьюхи апишки

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
-from django.views.generic import View
+from django.views.generic import View, DetailView, UpdateView
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from api_v1.serializers import ArticleSerializer
@@ -32,3 +32,30 @@ class ArticleCreateView(View):
             response = JsonResponse(slr.errors, safe=False)
             response.status_code = 400
             return response
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+        slr = ArticleSerializer(article, many=False)
+        return JsonResponse(slr.data)
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+        slr = ArticleSerializer(article=article)
+        if slr.is_valid():
+            article = slr.save()
+            return JsonResponse(slr.data, safe=False)
+        else:
+            response = JsonResponse(slr.errors, safe=False)
+            response.status_code = 400
+            return response
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+        slr = ArticleSerializer(article=article)
+        article = slr.delete()
+        return JsonResponse(pk=kwargs['pk'])
